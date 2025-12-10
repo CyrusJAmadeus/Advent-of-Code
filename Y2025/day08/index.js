0f2660f49eeefb746f069c99802442b45221d7ca
Amadeus <mwx.kaiser@gmail.com>
2025-12-10 07:25:18 +0330

    day8 is completed but the code is very messy

--- Y2025/day08/index.js
+++ Y2025/day08/index.js
@@ -1,22 +1,30 @@
 import fs from "node:fs";
-import { queryObjects } from "node:v8";
 
 try {
   const data = fs
-    .readFileSync("test.txt", "utf8")
-    .split("\r\n")
+    .readFileSync("input.txt", "utf8")
+    .split("\n")
     .map((line) => line.split(","));
 
-  const distances = findDistance(data);
-  const pairs = distances
-    .sort((a, b) => a[2] - b[2])
-    .slice(0, 10)
-    .map((triplet) => [triplet[0], triplet[1]]);
+  const groups = Array.from({ length: data.length }, (_, index) => index);
+  // console.log(groups);
 
-  findCircuts(pairs);
-  //   console.log(pairs);
+  const distances = findDistance(data);
+  const pairs = calcPairs(distances, 1000);
+  const allPairs = calcPairs(distances);
+
+  console.log(allPairs.length);
+  const [i, j] = findCircuts(allPairs, groups);
+  console.log(data[i][0] * data[j][0]);
 } catch (err) {
   console.error(err);
+}
+
+function calcPairs(dists, index = dists.length) {
+  return dists
+    .sort((a, b) => a[2] - b[2])
+    .slice(0, index)
+    .map((triplet) => [triplet[0], triplet[1]]);
 }
 
 function findDistance(list) {
@@ -35,12 +43,32 @@ function findDistance(list) {
   return distList;
 }
 
-function findCircuts(pairs) {
-  //   const keys = Array.from(new Set(pairs.map((pair) => pair[0])));
-  //   const neighbors = Array.from({ length: keys.length }, () => []);
-  //   for (let i = 0; i < keys.length; i++) {}
-  console.log({
-    ...Object.groupBy(pairs, (row) => row[0]),
-    ...Object.groupBy(pairs, (row) => row[1]),
-  });
+function findCircuts(pairs, groups) {
+  let links = 0;
+  function find(x) {
+    if (groups[x] === x) return x;
+    return (groups[x] = find(groups[x]));
+  }
+
+  function union(x, y) {
+    groups[find(y)] = find(x);
+  }
+
+  for (const pair of pairs) {
+    if (find(pair[0]) !== find(pair[1])) {
+      union(...pair);
+      links++;
+    }
+    if (links === groups.length - 1) return pair;
+  }
+
+  for (let i = 0; i < groups.length; i++) {
+    find(i);
+  }
+  const sortedGroupLength = Object.values(Object.groupBy(groups, (el) => el))
+    .map((value) => value.length)
+    .sort((a, b) => b - a);
+
+  const top3 = sortedGroupLength.slice(0, 3);
+  return top3.reduce((acc, cur) => acc * cur, 1);
 }
